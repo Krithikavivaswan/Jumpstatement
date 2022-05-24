34279bc1b5a617ef56d92312e343afbc50304d9b
krithikavivaswan <krithikaganesh@ymail.com>
2022-05-24 14:22:50 -0700

    New proj

---
+++ src/Jump/JumpStat.java
@@ -0,0 +1,57 @@
+package Jump;
+
+public class JumpStat {
+	public static void main(String[] args) {
+		// While loop /entry check loop
+		int i=0;
+//		while (i<=10) {
+//		System.out.println(i);	
+//		i++;
+//		}
+//		
+		
+		// Do while loop/ Exit 
+//		do {
+//			System.out.println(i);
+//			i++;
+//	
+//		} while (i<10);
+//		
+		// break
+		
+//		for (int j = 0; j < 16; j++) {
+//			if (j==5) {
+//				break;
+//			}
+//			System.out.println(j);
+//		}
+//		
+		
+		// continue
+//		for (int j = 0; j <=10; j++) {
+//		
+//			if (j==4) {
+//				continue;
+//				
+//		}
+//		
+//		System.out.println(j);
+//		}
+//		
+		//System.exit
+		
+		for (int j = 0; j <=5; j++) {
+			if (j==4) {
+				System.exit(0);
+		
+			}
+		System.out.println(j);
+		
+		}
+		System.out.println("Done");
+		
+	}
+	
+	
+
+}
